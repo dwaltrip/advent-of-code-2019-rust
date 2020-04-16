4b3e76cd9342513a990afbdf5e9a86928490560f
Daniel Waltrip <dwaltrip77@gmail.com>
2020-04-16 13:55:50 -0700

    [Day 6] Solve part 2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::fs;
+use std::fmt::{Debug};
 
 fn main() {
   let raw_input = fs::read_to_string("./puzzle-input.txt")
@@ -12,17 +13,61 @@ fn main() {
     .collect();
 
   let orbit_map = build_orbit_map(&orbit_declarations);
-  let count = count_orbits(&orbit_map);
 
+  solve_part_1(&orbit_map);
+  solve_part_2(&orbit_map);
+}
+
+fn solve_part_1(orbit_map: &OrbitMap) {
+  let count = count_orbits(&orbit_map);
   println!("count_orbits: {:?}", count);
 }
+
+fn solve_part_2(orbit_map: &OrbitMap) {
+  let you_parent = orbit_map.parent_lookup.get("YOU").unwrap();
+  let santa_parent = orbit_map.parent_lookup.get("SAN").unwrap();
+
+  let count = count_transfers(&you_parent, &santa_parent, &orbit_map);
+  println!("number of transfers: {:?}", count); 
+}
+
+
+fn count_transfers(a: &str, b: &str, orbit_map: &OrbitMap) -> u32 {
+  if a == b {
+    0
+  }
+  else {
+    let depth_a = orbit_map.depth_lookup.get(a).unwrap();
+    let depth_b = orbit_map.depth_lookup.get(b).unwrap();
+
+    let next_a =
+      if depth_a >= depth_b {
+        orbit_map.parent_lookup.get(a).unwrap()
+      }
+      else {
+        a 
+      };
+    let next_b =
+      if depth_b >= depth_a {
+        orbit_map.parent_lookup.get(b).unwrap()
+      }
+      else {
+        b
+      };
+
+    count_transfers(&next_a, &next_b, &orbit_map) +
+      if a == next_a { 0 } else { 1 } +
+      if b == next_b { 0 } else { 1 }
+  }
+}
+
 
 fn count_orbits(orbit_map: &OrbitMap) -> u32 {
   count_orbits_for(&orbit_map, "COM", 0)
 }
 
 fn count_orbits_for(orbit_map: &OrbitMap, target: &str, current_count: u32) -> u32 {
-  match orbit_map.children_for_parent.get(target) {
+  match orbit_map.children_lookup.get(target) {
     Some(children) => {
       let mut counts_for_children = 0;
       for child in children.iter() {
@@ -34,28 +79,74 @@ fn count_orbits_for(orbit_map: &OrbitMap, target: &str, current_count: u32) -> u
   }
 }
 
+
 fn build_orbit_map(orbit_declarations: &Vec<&str>) -> OrbitMap {
-  let mut children_for_parent: HashMap<String, Vec<String>> = HashMap::new();
+  let mut children_lookup: HashMap<String, Vec<String>> = HashMap::new();
+  let mut parent_lookup: HashMap<String, String> = HashMap::new();
 
   for orbit in orbit_declarations.iter() {
     let parts: Vec<&str> = orbit.split(")").collect();
     if parts.len() != 2 {
       panic!("Invalid orbit: {:?}", orbit);
     }
-    let parent = parts[0];
-    let child = parts[1];
+    let parent = parts[0].to_string();
+    let child = parts[1].to_string();
 
-    let children = children_for_parent
-      .entry(parent.to_string())
+    let children = children_lookup
+      .entry(parent.clone())
       .or_insert(Vec::new());
-    children.push(child.to_string());
+    children.push(child.clone());
+
+    parent_lookup.insert(child.clone(), parent.clone());
   }
 
-  OrbitMap { children_for_parent }
+  let mut depth_lookup: HashMap<String, u32> = HashMap::new();
+  calculate_depths("COM", 0, &children_lookup, &mut depth_lookup);
+
+  OrbitMap {
+    children_lookup,
+    parent_lookup,
+    depth_lookup,
+  }
 }
 
+fn calculate_depths(
+  object: &str,
+  depth: u32,
+  children_lookup: &HashMap<String, Vec<String>>,
+  mut depth_lookup: &mut HashMap<String, u32>,
+) {
+  if let Some(children) = children_lookup.get(object) {
+    for child in children.iter() {
+      depth_lookup.insert(child.to_string(), depth + 1);
+      calculate_depths(child, depth + 1, &children_lookup, &mut depth_lookup);
+    }
+  }
+}
+
+
 struct OrbitMap {
-  children_for_parent: HashMap<String, Vec<String>>,
+  children_lookup: HashMap<String, Vec<String>>,
+  parent_lookup: HashMap<String, String>,
+  depth_lookup: HashMap<String, u32>,
+}
+
+impl OrbitMap {
+  fn pretty_print(&self) {
+    println!("-- OrbitMap --");
+
+    fn print_map<T: Debug, U: Debug>(map: &HashMap<T, U>) {
+      for (key, value) in map.iter() {
+        println!("\t\t{:?} -> {:?}", key, value);
+      }
+    }
+    println!("\t children_lookup:");
+    print_map(&self.children_lookup);
+    println!("\t parent_lookup:");
+    print_map(&self.parent_lookup);
+    println!("\t depth_lookup:");
+    print_map(&self.depth_lookup);
+  }
 }
 
 
@@ -64,7 +155,7 @@ mod tests {
   use super::*;
 
   #[test]
-  fn name() {
+  fn test_count_orbits() {
     let orbit_declarations = vec![
      "COM)B",
      "B)C",
@@ -80,5 +171,19 @@ mod tests {
     ];
     let orbit_map = build_orbit_map(&orbit_declarations);
     assert_eq!(count_orbits(&orbit_map), 42);
+  }
+
+  #[test]
+  fn test_count_transfers() {
+    let orbit_declarations = vec![
+      "COM)A",
+      "A)B",
+      "A)C",
+      "C)D",
+      "D)E",
+      "D)F",
+    ];
+    let orbit_map = build_orbit_map(&orbit_declarations);
+    assert_eq!(count_transfers("B", "F", &orbit_map), 4);
   }
 }
