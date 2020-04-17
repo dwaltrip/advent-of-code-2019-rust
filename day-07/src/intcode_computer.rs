33ff42a4aefc21ac0448c23472e3a5b90244826d
Daniel Waltrip <dwaltrip77@gmail.com>
2020-04-16 23:16:20 -0700

    [Day 7] Use vec for program inputs. Minor naming fixes

--- day-07/src/intcode_computer.rs
+++ day-07/src/intcode_computer.rs
@@ -4,16 +4,18 @@ const DEBUG: bool = false;
 const MAX_LOOP_ITERATIONS: isize = 10_000;
 
 // Copied from Day 5
-pub fn run_program(values: &mut Vec<isize>, input: isize) -> Vec<isize> {
-  let mut output = Vec::new();
+pub fn run_program(values: &mut Vec<isize>, inputs: &Vec<isize>) -> Vec<isize> {
+  let mut inputs = inputs.clone();
+  inputs.reverse(); // this allows us to `pop` them
 
+  let mut output = Vec::new();
   let mut instruction_pointer = 0;
   let mut iteration_count = 0;
 
   if DEBUG {
     println!("");
     println!("-----------------------------");
-    println!("Input: {:?}", input);
+    println!("Inputs: {:?}", inputs);
     println!("Initial program: {:?}", values);
   }
 
@@ -100,7 +102,8 @@ pub fn run_program(values: &mut Vec<isize>, input: isize) -> Vec<isize> {
           get_param_val(&values, &params[1]);
       },
       Opcode::Input => {
-        values[params[0].value as usize] = input;
+        values[params[0].value as usize] = inputs.pop()
+          .expect("Ran out of inputs...");
       },
       Opcode::Output => {
         output.push(get_param_val(&values, &params[0]));
@@ -200,53 +203,53 @@ mod tests {
   #[test]
   fn works_with_op_codes_1_2_99() {
     struct Case {
-      input: Vec<isize>,
-      result: Vec<isize>,
+      program: Vec<isize>,
+      end_state: Vec<isize>,
     }
 
     // Example programs from Day 2
     let cases = vec![
       Case {
-        input:  vec![1,0,0,0,99],
-        result: vec![2,0,0,0,99],
+        program:   vec![1,0,0,0,99],
+        end_state: vec![2,0,0,0,99],
       },
       Case {
-        input:  vec![2,3,0,3,99],
-        result: vec![2,3,0,6,99],
+        program:   vec![2,3,0,3,99],
+        end_state: vec![2,3,0,6,99],
       },
       Case {
-        input:  vec![2,4,4,5,99,0],
-        result: vec![2,4,4,5,99,9801],
+        program:   vec![2,4,4,5,99,0],
+        end_state: vec![2,4,4,5,99,9801],
       },
       Case {
-        input:  vec![1,1,1,4,99,5,6,0,99],
-        result: vec![30,1,1,4,2,5,6,0,99],
+        program:   vec![1,1,1,4,99,5,6,0,99],
+        end_state: vec![30,1,1,4,2,5,6,0,99],
       },
       Case {
-        input:  vec![1,9,10,3,2,3,11,0,99,30,40,50],
-        result: vec![3500,9,10,70,2,3,11,0,99,30,40,50],
+        program:   vec![1,9,10,3,2,3,11,0,99,30,40,50],
+        end_state: vec![3500,9,10,70,2,3,11,0,99,30,40,50],
       },
     ];
 
     for case in cases {
-      let mut input = case.input.clone();
-      run_program(&mut input, 0);
-      assert_eq!(input, case.result);
+      let mut program = case.program.clone();
+      run_program(&mut program, &vec![0]);
+      assert_eq!(program, case.end_state);
     }
   }
 
   struct TestCase {
     program: Vec<isize>,
-    input: isize,
-    final_state: Vec<isize>,
+    inputs: Vec<isize>,
+    end_state: Vec<isize>,
     output: Vec<isize>,
   }
 
   fn run_test_cases(cases: &Vec<TestCase>) {
     for case in cases {
       let mut program = case.program.clone();
-      let output = run_program(&mut program, case.input);
-      assert_eq!(program, case.final_state);
+      let output = run_program(&mut program, &case.inputs);
+      assert_eq!(program, case.end_state);
       assert_eq!(output, case.output);
     }
   }
@@ -256,8 +259,8 @@ mod tests {
     let cases = vec![
       TestCase {
         program: vec![3,0,4,0,99],
-        input: 1,
-        final_state: vec![1, 0, 4, 0, 99],
+        inputs: vec![1],
+        end_state: vec![1, 0, 4, 0, 99],
         output: vec![1],
       },
     ];
@@ -272,15 +275,15 @@ mod tests {
       TestCase {
         // Address 8 is non-zero, so it does jump.
         program: vec![1005, 8, 5, 4, 9, 4, 10, 99, 1, -1, -2],
-        input: -1,
-        final_state: vec![1005, 8, 5, 4, 9, 4, 10, 99, 1, -1, -2],
+        inputs: vec![-1],
+        end_state: vec![1005, 8, 5, 4, 9, 4, 10, 99, 1, -1, -2],
         output: vec![-2],
       },
       TestCase {
         // Address 8 is 0, so it does NOT jump.
         program: vec![1005, 8, 5, 4, 9, 4, 10, 99, 0, -1, -2],
-        input: -1,
-        final_state: vec![1005, 8, 5, 4, 9, 4, 10, 99, 0, -1, -2],
+        inputs: vec![-1],
+        end_state: vec![1005, 8, 5, 4, 9, 4, 10, 99, 0, -1, -2],
         output: vec![-1, -2],
       },
     ];
@@ -295,15 +298,15 @@ mod tests {
       TestCase {
         // Address 8 is non-zero, so it does NOT jump.
         program: vec![1006, 8, 5, 4, 9, 4, 10, 99, 1, -1, -2],
-        input: -1,
-        final_state: vec![1006, 8, 5, 4, 9, 4, 10, 99, 1, -1, -2],
+        inputs: vec![-1],
+        end_state: vec![1006, 8, 5, 4, 9, 4, 10, 99, 1, -1, -2],
         output: vec![-1, -2],
       },
       TestCase {
         // Address 8 is 0, so it does jump.
         program: vec![1006, 8, 5, 4, 9, 4, 10, 99, 0, -1, -2],
-        input: -1,
-        final_state: vec![1006, 8, 5, 4, 9, 4, 10, 99, 0, -1, -2],
+        inputs: vec![-1],
+        end_state: vec![1006, 8, 5, 4, 9, 4, 10, 99, 0, -1, -2],
         output: vec![-2],
       },
     ];
@@ -320,40 +323,40 @@ mod tests {
       // position mode (1st bulleted example in part 2)
       TestCase {
         program: program1.clone(),
-        input: 8, // equals 8
-        final_state: vec![3,9,8,9,10,9,4,9,99,1,8],
+        inputs: vec![8], // equals 8
+        end_state: vec![3,9,8,9,10,9,4,9,99,1,8],
         output: vec![1],
       },
       TestCase {
         program: program1.clone(),
-        input: 5, // less than 8
-        final_state: vec![3,9,8,9,10,9,4,9,99,0,8],
+        inputs: vec![5], // less than 8
+        end_state: vec![3,9,8,9,10,9,4,9,99,0,8],
         output: vec![0],
       },
       TestCase {
         program: program1.clone(),
-        input: 900, // greater than 8
-        final_state: vec![3,9,8,9,10,9,4,9,99,0,8],
+        inputs: vec![900], // greater than 8
+        end_state: vec![3,9,8,9,10,9,4,9,99,0,8],
         output: vec![0],
       },
 
       // immediate mode (3rd bulleted example in part 2)
       TestCase {
         program: program2.clone(),
-        input: 8, // equals 8,
-        final_state: vec![3,3,1108,1,8,3,4,3,99],
+        inputs: vec![8], // equals 8,
+        end_state: vec![3,3,1108,1,8,3,4,3,99],
         output: vec![1],
       },
       TestCase {
         program: program2.clone(),
-        input: 5, // less than 8,
-        final_state: vec![3,3,1108,0,8,3,4,3,99],
+        inputs: vec![5], // less than 8,
+        end_state: vec![3,3,1108,0,8,3,4,3,99],
         output: vec![0],
       },
       TestCase {
         program: program2.clone(),
-        input: 900, // greater than 8,
-        final_state: vec![3,3,1108,0,8,3,4,3,99],
+        inputs: vec![900], // greater than 8,
+        end_state: vec![3,3,1108,0,8,3,4,3,99],
         output: vec![0],
       },
     ];
