c5bd02590d7c52aa0f6d2619c7df42c754d140ea
Daniel Waltrip <dwaltrip77@gmail.com>
2020-04-24 13:39:30 -0700

    [Day 8] Clean up and code improvements

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -17,22 +17,18 @@ fn main() {
   solve_part_2(&layers);
 }
 
-fn solve_part_2(layers: &Vec<Vec<u32>>) {
+fn solve_part_2(layers: &Vec<&[u32]>) {
   let mut decoded_img: Vec<u32> = Vec::new();
 
   let layer_size = layers[0].len();
   for i in 0..layer_size {
-    let mut done = false;
-    for layer in layers.iter() {
-      if layer[i] != TRANSARENT {
-        decoded_img.push(layer[i]);
-        done = true;
-        break
-      }
-    }
-    if !done {
-      decoded_img.push(TRANSARENT);
-    }
+    // Question: Is this slower than a manual for loop?
+    decoded_img.push(layers
+      .iter()
+      .map(|layer| layer[i])
+      .find(|pixel| *pixel != TRANSARENT)
+      .unwrap_or(TRANSARENT)
+    );
   }
 
   println!("");
@@ -47,17 +43,11 @@ fn solve_part_2(layers: &Vec<Vec<u32>>) {
 }
 
 
-fn solve_part_1(layers: &Vec<Vec<u32>>) {
-  let mut min_zero_count = u32::max_value();
-  let mut layer_with_min_zeros: &Vec<u32> = &layers[0];
-
-  for layer in layers.iter() {
-    let num_zeros = count_num(&layer, 0);
-    if num_zeros < min_zero_count {
-      min_zero_count = num_zeros;
-      layer_with_min_zeros = layer;
-    }
-  }
+fn solve_part_1(layers: &Vec<&[u32]>) {
+  let layer_with_min_zeros = layers
+    .iter()
+    .min_by_key(|layer| count_num(&layer, 0))
+    .unwrap();
 
   println!(
     "Part 1 Answer: {:?}",
@@ -65,27 +55,12 @@ fn solve_part_1(layers: &Vec<Vec<u32>>) {
   );
 }
 
-fn count_num(nums: &Vec<u32>, target: u32) -> u32 {
-  // This doesn't work, not sure why:
-  //   nums.iter().filter(|x| **x= target).collect().len()
-  let mut count = 0;
-  for num in nums {
-    if *num == target {
-      count += 1;
-    }
-  }
-  count
+fn count_num(nums: &[u32], target: u32) -> u32 {
+  nums.iter().filter(|&x| *x == target).count() as u32
 }
 
-fn parse_layers(input: &Vec<u32>, width: u32, height: u32) -> Vec<Vec<u32>> {
+fn parse_layers(input: &Vec<u32>, width: u32, height: u32) -> Vec<&[u32]> {
   let layer_size: usize = (width * height) as usize;
   assert!(input.len() % layer_size == 0);
-
-  let num_layers = input.len() / layer_size;
-  let mut layers = Vec::new();
-  for z in 0..num_layers {
-    let start = z * layer_size;
-    layers.push(input[start..start+layer_size].to_vec())
-  }
-  layers
+  input.chunks(layer_size).collect()
 }
