fadabb34e41c6d8b5599443437b9e07f2df1682c
Daniel Waltrip <dwaltrip77@gmail.com>
2020-06-10 22:21:39 -0700

    [Day 10] Part 2 wip -- good progress

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,25 +1,156 @@
-use std::cmp;
 use std::collections::{ HashMap, HashSet };
 use std::fs;
+use std::hash::{Hash, Hasher};
+use std::cmp::{Eq, PartialEq};
 
-type Point = (usize, usize);
+type Point = (isize, isize);
 
 struct AsteroidMap {
   asteroids: HashSet<Point>,
-  height: usize,
-  width: usize,
 }
 
 fn main() {
   let map = parse_asteroid_map(
     &fs::read_to_string("./puzzle-input.txt").unwrap()
   );
-
-  solve_part_1(&map);
+  // solve_part_1(&map);
+  solve_part_2(&map);
 }
 
+#[allow(dead_code)]
 fn solve_part_1(map: &AsteroidMap) {
   println!("best position: {:?}", find_best_observing_pos(&map));
+}
+
+#[allow(dead_code)]
+fn solve_part_2(map: &AsteroidMap) {
+  let (station_pos, _) = find_best_observing_pos(&map);
+  println!(
+    "200th asteroid: {:?}", 
+    &find_nth_asteroid_to_blast(&map, station_pos, 200),
+  );
+}
+
+fn find_nth_asteroid_to_blast(map: &AsteroidMap, station_pos: Point, n: usize) -> Option<Point> {
+  let asteroid_coords: Vec<PolarCoord> = map.asteroids
+    .iter()
+    .filter(|&p| *p != station_pos)
+    .map(|p| PolarCoord::from_point(relativize_point(&station_pos, &p)))
+    .collect();
+
+  let mut asteroids_by_angle = gather_by_angle_and_sort_by_distance(&asteroid_coords);
+  let angles = sort_angles_clockwise_starting_at_90(
+    &asteroids_by_angle.keys().cloned().collect::<Vec<Angle>>()
+  );
+
+  let mut count = 0;
+  loop {
+    let mut done = true;
+
+    for angle in angles.iter() {
+      if let Some(coords) = asteroids_by_angle.get_mut(angle) {
+        if let Some(coord) = coords.pop() {
+          count += 1;
+          if count == n {
+            return Some(unrelativize_coords(&station_pos, &coord.point));
+          }
+        }
+      }
+      if asteroids_by_angle.get(angle).map_or(0, |coords| coords.len()) > 0 {
+        done = false;
+      }
+    }
+
+    if done {
+      break;
+    }
+  }
+
+  None
+}
+
+#[derive(Clone)]
+#[derive(Debug)]
+struct PolarCoord {
+  angle: Angle,
+  distance: f64,
+  point: Point,
+}
+
+#[derive(Clone)]
+#[derive(Debug)]
+struct Angle {
+  value: f64,
+  rise: isize,
+  run: isize,
+}
+
+impl Angle {
+  fn from_point(point: Point) -> Angle {
+    let gcd = gcd(point.0, point.1) as isize;
+    Angle {
+      value: angle360(point.1 as f64, point.0 as f64),
+      rise: point.1 / gcd,
+      run: point.0 / gcd,
+    }
+  }
+}
+
+impl Hash for Angle {
+  fn hash<H: Hasher>(&self, state: &mut H) {
+    self.rise.hash(state);
+    self.run.hash(state);
+  }
+}
+
+impl PartialEq for Angle {
+  fn eq(&self, other: &Self) -> bool {
+    self.rise == other.rise && self.run == other.run
+  }
+}
+
+impl Eq for Angle {}
+
+impl PolarCoord {
+  fn from_point(point: Point) -> PolarCoord {
+    let (x, y) = point;
+    PolarCoord {
+      angle: Angle::from_point(point),
+      distance: ((x.pow(2) + y.pow(2)) as f64).sqrt(),
+      point,
+    }
+  }
+}
+
+impl PartialEq for PolarCoord {
+  fn eq(&self, other: &Self) -> bool {
+    self.angle == other.angle && self.distance == other.distance
+  }
+}
+
+impl Eq for PolarCoord {}
+
+fn relativize_point(relative_origin: &Point, point: &Point) -> Point {
+  (
+    point.0 - relative_origin.0, 
+    point.1 - relative_origin.1, 
+  )
+}
+
+fn unrelativize_coords(relative_origin: &Point, point: &Point) -> Point {
+  (
+    point.0 + relative_origin.0, 
+    point.1 + relative_origin.1, 
+  )
+}
+
+fn angle360(y: f64, x: f64) -> f64 {
+  let mut theta = y.atan2(x) * (180.0 / std::f64::consts::PI);
+  // normalize to range: [0, 360)
+  if theta < 0.0 {
+    theta += 360.0;
+  }
+  theta
 }
 
 fn find_best_observing_pos(map: &AsteroidMap) -> (Point, usize) {
@@ -69,7 +200,7 @@ fn get_points_on_line(p1: Point, p2: Point) -> Vec<Point> {
 
   let slope_x: isize = p2.0 as isize - p1.0 as isize;
   let slope_y: isize = p2.1 as isize - p1.1 as isize;
-  let slope_gcd = gcd(slope_x.abs() as usize, slope_y.abs() as usize) as isize;
+  let slope_gcd = gcd(slope_x, slope_y) as isize;
 
   if slope_gcd == 1 {
     return vec![];
@@ -86,14 +217,14 @@ fn get_points_on_line(p1: Point, p2: Point) -> Vec<Point> {
       // the x values are the same, could use either point
       vec![p1.0; y_dist-1]
     } else {
-      get_nums_in_between(p1.0, p2.0, x_step)
+      get_nums_in_between(isize_to_usize(p1.0), isize_to_usize(p2.0), x_step)
     };
   let y_vals = 
     if slope_y == 0 {
       // y values are the same, could use either point
       vec![p1.1; x_dist-1]
     } else {
-      get_nums_in_between(p1.1, p2.1, y_step)
+      get_nums_in_between(isize_to_usize(p1.1), isize_to_usize(p2.1), y_step)
     };
   assert!(x_vals.len() == y_vals.len());
 
@@ -109,7 +240,7 @@ fn get_points_on_line(p1: Point, p2: Point) -> Vec<Point> {
 // TODO: I'm sure there is a nicer way of doing this...
 // Maybe using Range somehow. But this works well, is easy
 // to understand, and the types are simple.
-fn get_nums_in_between(a: usize, b: usize, step: isize) -> Vec<usize> {
+fn get_nums_in_between(a: usize, b: usize, step: isize) -> Vec<isize> {
   assert!(a != b);
   assert!(if a < b { step > 0 } else { step < 0 });
   let a = a as isize;
@@ -120,7 +251,7 @@ fn get_nums_in_between(a: usize, b: usize, step: isize) -> Vec<usize> {
 
   let mut curr = a + step;
   while is_not_done(curr) {
-    nums.push(isize_to_usize(curr));
+    nums.push(curr);
     curr += step;
   }
   nums
@@ -128,27 +259,45 @@ fn get_nums_in_between(a: usize, b: usize, step: isize) -> Vec<usize> {
 
 fn parse_asteroid_map(input_string: &str) -> AsteroidMap {
   let mut asteroids = HashSet::new();
-  let mut height: usize = 0;
-  let mut width: usize = 0;
 
   input_string
     .trim()
     .split('\n')
     .enumerate()
     .for_each(|(y, line)| {
-      height = cmp::max(y, height);
       line.trim().chars().enumerate().for_each(|(x, c)| {
         if c == '#' {
-          asteroids.insert((x,y));
+          asteroids.insert((x as isize, y as isize));
         }
-        width = cmp::max(x, width);
       });
     });
 
-  AsteroidMap { asteroids, height, width }
+  AsteroidMap { asteroids }
 }
 
-fn gcd(a: usize, b: usize) -> usize {
+fn gather_by_angle_and_sort_by_distance(coords: &[PolarCoord]) -> HashMap<Angle, Vec<PolarCoord>> {
+  let mut map = HashMap::new();
+
+  for coord in coords.iter() {
+    map
+      .entry(coord.angle.clone())
+      .or_insert(Vec::new())
+      .push(coord.clone());
+  }
+  for (_, coords_at_angle) in map.iter_mut() {
+    coords_at_angle.sort_by(|a, b| a.distance.partial_cmp(&b.distance).unwrap());
+    coords_at_angle.reverse();
+  } 
+
+  map
+}
+
+// -----------------------------------------------------------------------------
+
+fn gcd(a: isize, b: isize) -> isize {
+  let a = a.abs();
+  let b = b.abs();
+
   if a == 0 {
     b
   }
@@ -166,6 +315,21 @@ fn isize_to_usize(num: isize) -> usize {
     panic!(format!("Failed to convert: {:?}", num));
   }
   num as usize
+}
+
+fn sort_angles_clockwise_starting_at_90(angles: &[Angle]) -> Vec<Angle> {
+  fn adjust_angle(theta: f64) -> f64 {
+    if theta > 90.0 { theta - 90.0 } else { theta + 270.0 }
+  }
+
+  let mut copy = angles.to_vec();
+  copy.sort_by(|a, b| {
+    let a_val = adjust_angle(a.value);
+    let b_val = adjust_angle(b.value);
+    a_val.partial_cmp(&b_val).unwrap()
+  });
+  copy.reverse();
+  copy
 }
 
 // -----------------------------------------------------------------------------
@@ -200,8 +364,6 @@ mod tests {
 
     let map = parse_asteroid_map(&input_string);
     assert_eq!(map.asteroids, expected);
-    assert_eq!(map.height, 4);
-    assert_eq!(map.width, 4);
   }
 
   #[test]
@@ -317,5 +479,100 @@ mod tests {
         (case.best_pos, case.number_visible),
       );
     }
+  }
+
+  #[test]
+  fn test_gather_by_angle_and_sort_by_distance() {
+    let points = vec![
+      (0, 2),
+      (-2, 4),
+      (-1, 2),
+      (-2, 2),
+
+      (-1, 0),
+      (-3, 0),
+      (-2, -4),
+      (-2, -3),
+
+      (1, -1),
+      (3, -1),
+      (6, -2),
+
+      (2, 1),
+      (3, 0),
+      (3, 3),
+      (2, 2),
+    ];
+    let points_as_coords: Vec<PolarCoord> = points
+      .iter()
+      .map(|&p| PolarCoord::from_point(p))
+      .collect();
+
+    let mut expected = HashMap::new();
+    let mut add_entry = |key_point: Point, points: Vec<Point>| {
+      let p_coord = PolarCoord::from_point(key_point.clone());
+      expected.insert(
+        p_coord.angle.clone(),
+        points.iter().map(|&p| PolarCoord::from_point(p)).collect(),
+      );
+    };
+
+    add_entry((0, 2), vec![(0, 2)]);
+    add_entry((-1, 2), vec![(-2, 4), (-1, 2)]);
+    add_entry((-2, 2), vec![(-2, 2)]);
+
+    add_entry((-1, 0), vec![(-3, 0), (-1, 0)]);
+    add_entry((-2, -3), vec![(-2, -3)]);
+    add_entry((-2, -4), vec![(-2, -4)]);
+
+    add_entry((1, -1), vec![(1, -1)]);
+    add_entry((3, -1), vec![(6, -2), (3, -1)]);
+
+    add_entry((3, 0), vec![(3, 0)]);
+    add_entry((2, 1), vec![(2, 1)]);
+    add_entry((2, 2), vec![(3, 3), (2, 2)]);
+
+    let actual = gather_by_angle_and_sort_by_distance(&points_as_coords);
+
+    assert_eq!(actual.len(), expected.len());
+    assert_eq!(actual, expected);
+  }
+
+  #[test]
+  fn test_find_nth_asteroid_to_blast() {
+    let input = String::from("
+      .#....#####...#..
+      ##...##.#####..##
+      ##...#...#.#####.
+      ..#.....X...###..
+      ..#.#.....#....##
+    ");
+    let station_pos = (8, 3);
+
+    let map = parse_asteroid_map(&input);
+
+    assert_eq!(
+      find_nth_asteroid_to_blast(&map, station_pos, 9),
+      Some((15, 1)),
+    );
+  }
+
+  #[test]
+  fn test_sort_angles_clockwise_starting_at_90() {
+    let points = vec![
+      (0, 2),
+      (2, 1),
+      (3, 0),
+      (3, -1),
+      (0, -2),
+      (-2, -3),
+      (-3, 0),
+      (-2, 1), 
+      (-1, 3), 
+    ];
+
+    let angles: Vec<Angle> = points.iter().map(|&p| Angle::from_point(p)).collect();
+    let sorted = sort_angles_clockwise_starting_at_90(&angles);
+    assert_eq!(angles, sorted);
   }
 }
